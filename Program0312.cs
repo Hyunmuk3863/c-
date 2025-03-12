6c45dd7936cd9b249d0a0ff3c69b1e7d8dcbde78
Hyunmuk3863 <hm_3863@naver.com>
2025-03-12 21:48:53 +0900

    Add files via upload

---
+++ Program0312.cs
@@ -0,0 +1,49 @@
+﻿
+
+namespace ConsoleApp1
+{
+    internal class Program0312
+    {
+        static void Main(string[] args)
+         {
+            #region p10
+            /* byte a = 240;
+             Console.WriteLine($"a={a}");
+
+             byte b = 0b1111_0000;
+             Console.WriteLine($"b={b}");
+
+             byte c = 0XF0;
+             Console.WriteLine($"c={c}");
+
+             uint d = 0x1234_abcd;
+             Console.WriteLine($"d={d}");*/
+            #endregion
+
+            #region p11
+            //byte a = 128;
+            //sbyte b = (sbyte)a;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            #endregion
+
+            #region p12
+            int a = int.MaxValue;
+
+            Console.WriteLine(a);
+
+            a = a + 2;
+
+            Console.WriteLine(a);
+            #endregion
+
+            #region p13
+            /*float a = 3.1415_9625_3589_7932_3846f;
+            Console.WriteLine(a);
+
+            double b = 3.1415_9265_3589_7932_3846;
+            Console.WriteLine(b);*/
+            #endregion
+        }
+    }
+}
